cd4adb19e6aa2c21e4267f74bf84c095ac1ceb1e
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-26 14:12:53 +0800

    [fizzbuzz] [Carol] [test: test case with failed result which return 5]

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -29,6 +29,18 @@ public class FizzBuzzTest {
         //then
         assertEquals("Fizz", actual);
     }
+    @Test
+    public void should_return_buzz_when_count_off_given_order_number_multiple_of_5() throws Exception {
+        //given
+        int order = 5;
+        FizzBuzz result = new FizzBuzz();
+
+        //when
+        String actual = result.countOff(order);
+
+        //then
+        assertEquals("Buzz", actual);
+    }
 
 
 }
