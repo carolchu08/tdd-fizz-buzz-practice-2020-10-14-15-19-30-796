c441693c44e2090444790bdc49af9c322daf7898
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-26 14:05:53 +0800

    [fizzbuzz] [ Carol] [feat:  the actual result match expected result which return common case]

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void should_return_1_when_count_off_given_order_number_1() throws Exception {
-        //gives
+        //given
          int order= 1;
          FizzBuzz result = new FizzBuzz();
 
