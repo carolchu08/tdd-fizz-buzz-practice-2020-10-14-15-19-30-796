655e1575ed54168a04c04bf17805ccab9d991ef7
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-26 14:08:51 +0800

    [fizzbuzz] [Carol] [test: test case with failed result, given 3 instead of Fizz]

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -8,13 +8,27 @@ public class FizzBuzzTest {
     @Test
     public void should_return_1_when_count_off_given_order_number_1() throws Exception {
         //given
-         int order= 1;
-         FizzBuzz result = new FizzBuzz();
+        int order = 1;
+        FizzBuzz result = new FizzBuzz();
 
         //when
-         String actual =result.countOff(order);
+        String actual = result.countOff(order);
 
         //then
-        assertEquals("1",actual);
+        assertEquals("1", actual);
     }
+    @Test
+    public void should_return_fizz_when_count_off_given_order_number_multiple_of_3() throws Exception {
+        //given
+        int order = 3;
+        FizzBuzz result = new FizzBuzz();
+
+        //when
+        String actual = result.countOff(order);
+
+        //then
+        assertEquals("Fizz", actual);
+    }
+
+
 }
