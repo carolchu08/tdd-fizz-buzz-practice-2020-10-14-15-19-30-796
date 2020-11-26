4f8e2b2017f7626f7d4559e7285e48b4f46b5b63
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-26 14:29:14 +0800

    [fizzbuzz] [Carol] [feat: can return fizzwhizz when order is FizzBuzzWhizz]

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -7,9 +7,12 @@ public class FizzBuzz {
     public static final String WHIZZ = "Whizz";
     public static final String FIZZ_BUZZ = "FizzBuzz";
     public static final String FIZZ_WHIZZ = "FizzWhizz";
+    public static final String FIZZ_BUZZ_WHIZZ = "FizzBuzzWhizz";
 
     public String countOff(int order) {
-        if(order%3 == 0 && order%5 == 0){
+        if(order%3 == 0 && order%5 == 0 && order%7 ==0){
+            return FIZZ_BUZZ_WHIZZ;
+        }else if(order%3 == 0 && order%5 == 0){
             return FIZZ_BUZZ;
         }else if(order%3 == 0 && order%7 == 0){
             return FIZZ_WHIZZ;
