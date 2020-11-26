5e55e9dcfaff1809ac929c21befd4720bef97412
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-26 14:11:25 +0800

    [fizzbuzz] [Carol] [feat: Can return expected result : Fizz ]

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,13 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+
     public String countOff(int order) {
+        if(order%3 == 0){
+            return FIZZ;
+        }
         return String.valueOf(order);
     }
 }
